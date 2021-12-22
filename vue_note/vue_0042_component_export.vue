93948b15fad6f34ad5c589d056e66daa892d76f4
zhangkunjie <907535517@qq.com>
2021-12-22 19:54:17 +0800

    组件注册

---
+++ vue_note/vue_0042_component_export.vue
@@ -0,0 +1,24 @@
+<head>
+  <script src="https://cdn.jsdelivr.net/npm/vue@3"></script>
+</head>
+<template>
+  <div>
+    <p class="red fsize">Hello {{ names }}</p>
+  </div>
+</template>
+<script>
+//局部组件
+export default {
+  data: function () {
+    {
+      return {
+        names: "张一",
+      };
+    }
+  },
+};
+</script>
+<style  scoped>
+.red{color:red}
+.fsize{font-size:2em}
+</style>
---
+++ vue_note/vue_0043_component_import.vue
@@ -0,0 +1,11 @@
+<head>
+  <script src="https://cdn.jsdelivr.net/npm/vue@3"></script>
+</head>
+import Hello from './vue_0042_component_export.vue';
+//引入局部组件
+export default {
+};
+<template>
+    <Hello></Hello>
+</template>
+<script>
