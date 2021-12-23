12a7d1db6df3a3397103934f39dae3a8866cf5a4
zhangkunjie <907535517@qq.com>
2021-12-23 16:55:34 +0800

    单文件组件导入导出

--- vue_note/vue_0042_component_export.vue
+++ vue_note/vue_0042_component_export.vue
@@ -1,23 +1,20 @@
-<head>
-  <script src="https://cdn.jsdelivr.net/npm/vue@3"></script>
-</head>
 <template>
   <div>
-    <p class="red fsize">Hello {{ student_name }}</p>
+    <p class="red fsize">我是第一个组件的内容:{{ student_name }}</p>
   </div>
 </template>
 <script>
 //局部组件
 export default {
-  name:'hello',
+  name:'Hello',
   data: function () {
     {
       return {
-        student_name: "张一",
-      };
+        student_name: "张一"
+      }
     }
-  },
-};
+  }
+}
 </script>
 <style  scoped>
 .red{color:red}
--- vue_note/vue_0043_component_import.vue
+++ vue_note/vue_0043_component_import.vue
@@ -1,17 +1,22 @@
-<head>
-  <script src="https://cdn.jsdelivr.net/npm/vue@3"></script>
-</head>
 <script>
-import Hello from './vue_0042_component_export.vue';
-//引入局部组件
+import Hello from './vue_0042_component_export.vue'
 export default {
-  name:'myApp',
+  name:'Second',
+  data: function () {
+    {
+      return {
+        msg: "第二个组件的内容"
+      }
+    }
+  },
   components:{
-       Hello
+    Hello
   }
-};
+} 
 </script>
 <template>
-    <hello></hello>
+  <div>
+    {{msg}}
+    引入的第一个组件的内容:<Hello></Hello>
+  </div>
 </template>
-<script>
