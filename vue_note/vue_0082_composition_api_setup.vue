b5e47629ba4d7d540acdaf33218f31e6825f83c0
zhangkunjie <907535517@qq.com>
2022-01-19 21:00:53 +0800

    组合式api

---
+++ vue_note/vue_0082_composition_api_setup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <div>{{counter}}</div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default{
+  setup(){
+    const counter = ref(0);
+    return {counter};
+  }
+}
+</script>
